e4ab45be5a53536b6efa9268e994000bb6456ed6
tthu38 <ohtthu0308@gmail.com>
2025-09-17 13:31:13 +0700

    ok

---
+++ src/main/java/com/example/cinemaster/CineMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemaster/CineMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
