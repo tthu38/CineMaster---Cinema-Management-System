a8588aa080b9f4593efc0b7cc4622dd3574cb3fb
giangehehe365 <gianggiangnguyen010203@gmail.com>
2025-09-19 11:54:53 +0700

    Update Product.java
    giang

--- src/main/java/com/example/cinemaster/entity/Product.java
+++ src/main/java/com/example/cinemaster/entity/Product.java
@@ -2,4 +2,12 @@ package com.example.cinemaster.entity;
 
 public class Product {
     private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
