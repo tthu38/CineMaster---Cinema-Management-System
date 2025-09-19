601832b5cde9a575c70375325b71546c4e004ac9
giangehehe365 <gianggiangnguyen010203@gmail.com>
2025-09-19 11:52:02 +0700

    Update Users.java
    giang

--- src/main/java/com/example/cinemaster/entity/Users.java
+++ src/main/java/com/example/cinemaster/entity/Users.java
@@ -13,4 +13,28 @@ public class Users {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
